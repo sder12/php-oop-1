3e43d9b3df644c02f458fefdd51aff62ea43acba
sder12 <sara.cetto@gmail.com>
2022-12-12 08:29:24 +0100

    first commit

---
+++ Models/Movie.php
@@ -0,0 +1,13 @@
+<?php
+
+class Movie
+{
+    public $title;
+    public $year;
+    public $duration;
+    public $country;
+    public $director;
+    public $cinematography;
+    public $genre;
+    public $cast;
+};
